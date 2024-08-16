0e1e6e217273ce23b3323e02b421da8db7913f1c
SOURAV7843 <souravsamanta.1352ss@gmail.com>
2024-08-16 18:53:35 +0530

    logger and exception code addes

--- signLanguage/exception/__init__.py
+++ signLanguage/exception/__init__.py
@@ -1,29 +1,34 @@
+##This code for where exception/error is raised
 import sys
 
-
 def error_message_detail(error, error_detail: sys):
+    # Extracts exception information: type, value, traceback
     _, _, exc_tb = error_detail.exc_info()
 
+    # Gets the filename where the exception occurred
     file_name = exc_tb.tb_frame.f_code.co_filename
 
-    error_message = "Error occurred python script name [{0}] line number [{1}] error message [{2}]".format(
+    # Creates a detailed error message including the script name, line number, and error message
+    error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name, exc_tb.tb_lineno, str(error)
     )
 
     return error_message
 
-
+# Custom exception class for handling errors with detailed information
 class SignException(Exception):
     def __init__(self, error_message, error_detail):
         """
-        :param error_message: error message in string format
+        :param error_message: Error message in string format
+        :param error_detail: Contains detailed info about the error, including traceback
         """
-        super().__init__(error_message)
+        super().__init__(error_message)  # Calls the base class constructor with the error message
 
+        # Generates detailed error message using the error_message_detail function
         self.error_message = error_message_detail(
             error_message, error_detail=error_detail
         )
 
     def __str__(self):
+        # Returns the detailed error message when the exception is printed
         return self.error_message
-    
--- signLanguage/logger/__init__.py
+++ signLanguage/logger/__init__.py
@@ -1,19 +1,25 @@
-import logging
-import os
-from datetime import datetime
-from from_root import from_root
+#This whole  code create the log message with timestamp
 
+import logging  # Importing the logging module for generating log messages.
+import os  # Importing the os module for interacting with the operating system.
+from datetime import datetime  # Importing datetime for working with dates and times.
+from from_root import from_root  # Importing from_root to get the root directory of the project.
 
+# Generate a log file name based on the current date and time.
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
+# Define the path where the log file will be stored, within a 'log' directory at the project root.
 log_path = os.path.join(from_root(), 'log', LOG_FILE)
 
+# Create the 'log' directory if it doesn't already exist.
 os.makedirs(log_path, exist_ok=True)
 
+# Define the full path to the log file, including its name.
 lOG_FILE_PATH = os.path.join(log_path, LOG_FILE)
 
+# Configure the logging settings.
 logging.basicConfig(
-    filename=lOG_FILE_PATH,
-    format= "[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s",
-    level= logging.INFO
+    filename=lOG_FILE_PATH,  # Set the log file's path.
+    format="[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s",  # Specify the log message format.
+    level=logging.INFO  # Set the logging level to INFO, so only INFO and higher level messages are logged.
 )
